c3c525a991a6fbb29d43042855a9af8a89831788
EatMyHitzz <49417077+EatMyHitzz@users.noreply.github.com>
2019-07-21 13:19:21 +0200

    Update serverlogo.js

--- serverlogo.js
+++ serverlogo.js
@@ -1,1 +1,15 @@
+const discord = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+
+   let sicon = message.guild.iconURL;
+   let serverembed = new Discord.RichEmbed()
+   .setImage(sicon)
+
+   message.channel.send(serverembed);
+
+}
+
+module.exports.help = {
+    name: "serverinfo"
+}
